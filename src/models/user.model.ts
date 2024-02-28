7f1314fd947b4b590e680a08c09db6dbd53912da
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-02-28 15:10:27 +0530

    Feat made auth routes and controllers

---
+++ src/models/user.model.ts
@@ -0,0 +1,21 @@
+import { Document, Schema, model } from "mongoose";
+
+interface IBaseUser {
+  name: string;
+  email: string;
+  password: string;
+  isAdmin: boolean;
+}
+
+interface IUserSchema extends IBaseUser, Document {}
+
+const userSchema = new Schema<IUserSchema>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
+});
+
+const userModel = model<IUserSchema>("user", userSchema);
+
+export default userModel;
